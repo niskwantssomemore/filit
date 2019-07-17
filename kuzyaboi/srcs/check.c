cf4a5ce3fbfaa3aab5afe89bbae3aa4e99ee07d4
Dannil2410 <tstripeb@oa-o3.21-school.ru>
2019-07-17 18:16:47 +0300

    check_s

--- kuzyaboi/srcs/check.c
+++ kuzyaboi/srcs/check.c
@@ -6,7 +6,7 @@
 /*   By: tstripeb <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/16 15:41:36 by tstripeb          #+#    #+#             */
-/*   Updated: 2019/07/17 13:14:06 by tstripeb         ###   ########.fr       */
+/*   Updated: 2019/07/17 18:16:14 by tstripeb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -64,6 +64,118 @@ static int ft_check_l(char *str)
 		flag++;
 	}
 	return (1);
+}
+
+static int ft_height_c(char *str)
+{
+	int index;
+	int counter;
+
+	index = 0;
+	counter = 0;
+	while (str[index])
+	{
+		while(str[index] && str[index] != '\n')
+		{
+			if (str[index] == '#')
+			{
+				counter++;
+				while (str[index] && str[index] != '\n')
+					index++;
+				break ;
+			}
+			index++;
+		}
+		if (!str[index])
+			break ;
+		index++;
+	}
+	return (counter);
+}
+
+static int ft_width_c(char *str)
+{
+	int index;
+	int counter;
+	int flag;
+
+	counter = 0;
+	flag = 1;
+	while (flag < 5)
+	{
+		index = 0;
+		while (index < 4)
+		{
+			if (str[flag + 5 * index - 1] == '#')
+			{
+				counter++;
+				break;
+			}
+			index++;
+		}
+		flag++;
+	}
+	return (counter);
+}
+
+static int ft_three_in_a_row(char *str, int index, int counter, int flag)
+{
+	if (counter == 6 && flag != 1)
+		return (counter);
+	else if (counter == 8)
+		return (counter);
+	if (str[index + 1] == '#')
+		counter = ft_three_in_a_row(str, index + 1, counter + 2);
+	else if (str[index + 5] == '#')
+		counter = ft_three_in_a_row(str, index + 5, counter + 2);
+	else if (str[index - 1] == '#')
+		counter = ft_three_in_a_row(str, index - 1, counter + 2);
+	else if (str[index - 5] == '#')
+		counter = ft_three_in_a_row(str, index - 5, counter + 2);
+}
+
+static int ft_neighbors_c(char *str, int height, int width)
+{
+	int index;
+	int counter;
+	int flag;
+
+	index = 0;
+	counter = 0;
+	flag = 0;
+	while (str[index])
+	{
+		while (str[index] != '\n' && str[index])
+		{
+			if (str[index] == '#' && height == 2 && width == 2)
+			{
+				flag = 1;
+				return ((ft_three_in_a_row(str, index, counter, flag) == 8) ? 1 : 0);
+			}
+			else if (str[index] == '#' && ((height == 1 && width == 3) ||
+						(width == 1 && height == 3)))
+				return ((ft_three_in_a_row(str, index, counter, flag) == 6 ) ? 1 : 0);
+			index++;
+		}
+		index++;
+	}
+	return (0);
+}
+
+static int ft_check_s(char *str)
+{
+	int height;
+	int width;
+
+	height = ft_height_c(str);
+	width = ft_width_c(str);
+	if ((height == 1 && width != 4) || (width == 1 && height != 4) ||
+			(height == 3 && width != 2) || (width == 3 && height != 2) ||
+			(height == 2 && width != 2) || (width == 2 && height != 2))
+		return (0);
+	if (ft_neighbors_c(str, height, width))
+		return (1);
+	return (0);
 }
 
 static int check(char **tetriminos)
