da315ae6e95c80f1f6a4cf1e64fb6ef551c7c728
Speeder Azalee <sazalee@oa-l1.21-school.ru>
2019-08-01 14:03:44 +0300

    solver

--- Makefile
+++ Makefile
@@ -6,10 +6,10 @@
 #    By: sazalee <marvin@42.fr>                     +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2019/07/10 16:19:16 by sazalee           #+#    #+#              #
-#    Updated: 2019/08/01 13:42:00 by sazalee          ###   ########.fr        #
+#    Updated: 2019/08/01 13:55:57 by sazalee          ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 NAME - fillit
 
-SRCS = main.c checker.c checkercont.c neighborscont.c read.c struct.c
+SRCS = main.c checker.c checkercont.c neighborscont.c read.c struct.c solver.c
--- srcs/solver.c
+++ srcs/solver.c
@@ -1,45 +1,15 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   solver.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: sazalee <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/07/30 12:30:01 by sazalee           #+#    #+#             */
-/*   Updated: 2019/08/01 13:55:01 by sazalee          ###   ########.fr       */
+/*   Created: 2019/08/01 13:53:16 by sazalee           #+#    #+#             */
+/*   Updated: 2019/08/01 13:53:38 by sazalee          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/fillit.h"
 
-void	ft_error(void)
-{
-	write(1, ERROR, 6);
-}
 
-int		main(int ac, char **av)
-{
-	t_tetris	*begin;
-	char		**base;
-
-	begin = NULL;
-	base = NULL;
-	if (ac != 2)
-	{
-		ft_error();
-		return (-1);
-	}
-	if ((base = ft_read(av[1])) == NULL)
-	{
-		ft_error();
-		return (-1);
-	}
-	if (!(check(base)))
-	{
-		ft_error();
-		фритайм(base, g_tetrinumber + 1);
-		return (-1);
-	}
-	begin = addtetri(base);
-	фритайм(base, g_tetrinumber + 1);
-}
