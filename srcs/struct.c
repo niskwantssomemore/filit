89d25d2792ff3a9115bca15ac0a465683a62ba94
Speeder Azalee <sazalee@oa-n2.21-school.ru>
2019-08-05 14:00:39 +0300

    lafin

--- srcs/struct.c
+++ srcs/struct.c
@@ -6,7 +6,7 @@
 /*   By: sazalee <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/01 12:27:06 by sazalee           #+#    #+#             */
-/*   Updated: 2019/08/01 13:15:21 by sazalee          ###   ########.fr       */
+/*   Updated: 2019/08/05 13:59:08 by sazalee          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,4 +46,47 @@ t_tetris	*addtetri(char **base)
 	}
 	temp->next = NULL;
 	return (begin);
+}
+
+int			ressize(void)
+{
+	int size;
+	int k;
+	int power;
+
+	size = g_tetrinumber * 4;
+	power = 0;
+	k = 1;
+	while (size > 0)
+	{
+		size -= k;
+		k = k + 2;
+		power++;
+	}
+	return (power);
+}
+
+char		**finalbase(int size)
+{
+	char	**base;
+	int		index;
+	int		jndex;
+
+	index = 0;
+	if (!(base = (char **)malloc(sizeof(char *) * size)))
+		return (0);
+	while (index < size)
+	{
+		jndex = 0;
+		if (!(base[index] = (char *)malloc(sizeof(char) * size + 1)))
+			return (0);
+		while (jndex < size)
+		{
+			base[index][jndex] = '.';
+			jndex++;
+		}
+		index++;
+	}
+	base[index] = NULL;
+	return (base);
 }
