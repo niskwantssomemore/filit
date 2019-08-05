1b9ae767df3420d5bdefc87817862fdcf230892b
Speeder Azalee <sazalee@oa-n2.21-school.ru>
2019-08-05 13:29:32 +0300

    checkmate

--- srcs/solver.c
+++ srcs/solver.c
@@ -6,11 +6,32 @@
 /*   By: sazalee <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/01 13:53:16 by sazalee           #+#    #+#             */
-/*   Updated: 2019/08/05 13:02:31 by sazalee          ###   ########.fr       */
+/*   Updated: 2019/08/05 13:28:15 by sazalee          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/fillit.h"
+
+char	**cleaner(t_tetris *begin, char **finalb)
+{
+	int	x;
+	int	y;
+
+	x = 0;
+	y = 0;
+	while (finalb[x] != NULL)
+	{
+		while (finalb[x][y] != '\0')
+		{
+			if (finalb[x][y] == begin->alphabet)
+				finalb[x][y] = '.';
+			y++;
+		}
+		y = 0;
+		x++;
+	}
+	return (finalb);
+}
 
 int		finder(t_tetris *begin, int finalsize, char **finalb)
 {
@@ -25,4 +46,5 @@ char	**solve(t_tetris *begin, int finalsize, char **finalb)
 		freetime(finalb, finalsize);
 		finalb = finalbase(finalsize);
 	}
+	return (finalb);
 }
