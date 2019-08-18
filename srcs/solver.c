7654837986aa93c297c78f3bc5e54864d6afc414
Speeder Azalee <sazalee@oa-h5.21-school.ru>
2019-08-18 15:51:13 +0300

    gotovo

--- srcs/solver.c
+++ srcs/solver.c
@@ -6,53 +6,56 @@
 /*   By: sazalee <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/01 13:53:16 by sazalee           #+#    #+#             */
-/*   Updated: 2019/08/06 14:28:44 by sazalee          ###   ########.fr       */
+/*   Updated: 2019/08/18 14:47:34 by sazalee          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/fillit.h"
 
-int		tester(t_tetris *begin, char **finalbase, int finalsize)
+int		tester(t_tetris *begin, char **finalb, int finalsize)
 {
 	int i;
 	int j;
 
 	i = 0;
+	j = 0;
 	while (i < 4)
 	{
-		j = 0;
 		while (j < 4)
 		{
 			if ((begin->tetrimino[i][j] == '#') && (begin->x + j >= finalsize
-						|| begin->y + i >= finalsize)
-					&& (finalbase[begin->y + i][begin->x + j] >= 'A' &&
-						finalbase[begin->y + i][begin->x + j] <= 'Z'))
+						|| begin->y + i >= finalsize))
+				return (0);
+			if ((begin->tetrimino[i][j] == '#') && (finalb[begin->y + i][begin->x + j] >= 'A' &&
+						finalb[begin->y + i][begin->x + j] <= 'Z'))
 				return (0);
 			j++;
 		}
+		j = 0;
 		i++;
 	}
 	return (1);
 }
 
-char	**adder(t_tetris *begin, char **finalbase)
+char	**adder(t_tetris *begin, char **finalb)
 {
 	int i;
 	int j;
 
 	i = 0;
+	j = 0;
 	while (i < 4)
 	{
-		j = 0;
 		while (j < 4)
 		{
 			if (begin->tetrimino[i][j] == '#')
-				finalbase[begin->x + i][begin->y + j] = begin->alphabet;
+				finalb[begin->y + i][begin->x + j] = begin->alphabet;
 			j++;
 		}
+		j = 0;
 		i++;
 	}
-	return (finalbase);
+	return (finalb);
 }
 
 char	**cleaner(t_tetris *begin, char **finalb)
